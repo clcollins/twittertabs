90e540f6575b43c057efd75fcd62317c3de65734
Chris Collins <chris@hastur.oit.duke.edu>
2014-03-25 12:39:31 -0400

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,35 @@ twittertabs
 ===========
 
 Simple app for keeping track of friend's activity on Twitter
+
+Visit Twitter to read the masses; get keep tabs on your friends' tweets with Twittertabs.
+
+Comments and suggestions welcome. Patches too!
+
+Usage
+-----
+
+Sign up for an app at dev.twitter.com/apps, and record your API and Access Keys and Secrets.
+
+Copy the ttabs.conf-sample file to ttabs.conf, and fill out the sections with your API and Access Keys and Secrets, and add the users you want to follow as a comma-separated list under 'users'.
+
+After doing that, running the script should give you a list of Tweets that are less than 5 hours old posted by the users you specified.
+
+Requirements
+------------
+
+* Python 2.7
+* Tweepy module
+* Time module
+* ConfigParser module
+
+Copyright Information
+---------------------
+
+Copyright (C) 2014 Chris Collins
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
---
+++ twittertabs.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+#
+# Twittertabs
+# ===========
+#
+# Keep track of specific Friend's tweets so they're not lost
+# in the masses
+#
+# Chris Collins, <christopher.collins@duke.edu>
+#
+# v0.1 - 2014-03-25
+#
+# Copyright (C) 2014 Chris Collins
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Read Auth and Friend list from config file
+# Return results of friends have posted in last 5 hours
+# Run via cron 4x/day
+
+# IMPORT FUNCTIONS
+# time - for checking age of tweets
+# tweepy -  for the Twitter OAuth interface <- replace with real oauth someday
+# ConfigParser - for config file reading
+
+import time
+import tweepy
+import ConfigParser
+
+# The Current Time
+now = time.time()
+
+# Parse the config file
+parser = ConfigParser.SafeConfigParser()
+parser.read('ttabs.conf')
+config = dict((section, dict((option, parser.get(section, option))
+                             for option in parser.options(section)))
+              for section in parser.sections())
+
+# The users to check for new tweets
+# Super ugly.  I should be ashamed.
+users = [config['users']['users']][0].split(',')
+
+# Keys/Tokens/Secrets for connecting to the Twitter API
+API_KEY = config['api']['key']
+API_SECRET = config['api']['secret']
+ACCESS_TOKEN = config['access']['key']
+ACCESS_SECRET = config['access']['secret']
+
+
+# Connect to twitter API
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+def check_tweets(user):
+    """ Get Tweets from this user """
+    for tweet in api.user_timeline(user):
+        """ If Tweet epoch time is greater than
+            the current epoch time minus five hours
+            (18000 seconds), then print """
+        createdstr = str(tweet.created_at)
+        if mkepoch(createdstr) > (int(now) - 18000):
+            print tweet.text
+            print '----------'
+            print tweet.author.screen_name
+            print tweet.created_at
+            print ''
+
+
+def mkepoch(date):
+    """ Convert the 'created_at' datetime to epoch """
+    pattern = '%Y-%m-%d %H:%M:%S'
+    epoch = int(time.mktime(time.strptime(date, pattern)))
+    return epoch
+
+
+for user in users:
+    check_tweets(user)
